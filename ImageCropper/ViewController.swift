ec614fcd176f452374e11938d543920a21c53a8b
Albert Bori <github@albertbori.com>
2018-09-29 10:12:28 -0600

    Initial Commit

---
+++ ImageCropper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageCropper
+//
+//  Created by Albert Bori on 9/29/18.
+//  Copyright © 2018 Albert Bori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
